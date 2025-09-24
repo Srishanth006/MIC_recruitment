ffbd3cfe2be3304c4f8db30807f6b6d3fd358da4
Srishanth <psrishanth2006@gmail.com>
2025-09-24 19:10:04 +0530

    setup gemini model 1.5 - flash and initialized prompt

---
+++ server/gemini.js
@@ -0,0 +1,15 @@
+const{GoogleGenerativeAI}=require("@google/generative-ai");
+const gemini=new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+async function generateResponse(mood){
+    try{
+        const model=gemini.getGenerativeModel({model:"gemini-1.5-flash"});
+        const prompt= 'Imagine you are music expert and you know most of the tamil songs ,According to the users mood : "${mood}",generate around 5-8 songs across 3 tamil artists that would fit this mood for spotify playlist,format the response as a valid JSON object in the format mentioned below ,only this nothing else :{"songs": ["song1", "song2", "song3"], "artists": ["artist_id1", "artist_id2"]}';
+        const result = await model.generateContent(prompt);
+        const responseText = await result.response;
+        const text=responseText.text();
+    }
+} catch(error){
+    console.error("Error generating songs from Gemini API:",error);
+    throw new Error("Failed to generate response");
+}
