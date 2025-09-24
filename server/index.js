a3b61d9835acd1afde77bbfb64da039a12011df3
Srishanth <psrishanth2006@gmail.com>
2025-09-24 18:58:02 +0530

    API rout setup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const server=express();
 const portno=process.env.PORT || 3000;
+app.use(express.json());
+app.post('api/MIC',(req,res)=>{
+    const{mood}=req.body;
+    if(!mood){
+        return res.status(400).json({error:"Mood is required"});
+    }
+    console.log('Mood fetched:',mood);
+    res.status(200).json({message:'Mood received successfully',mood:mood});
+});
+console.log('Mood fetched:',mood);
+
 server.listen(portno,()=>{
     console.log(`server started at port ${portno}`);
 });
